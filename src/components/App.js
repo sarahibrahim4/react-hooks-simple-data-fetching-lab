a3a9f06497af8c6a8c142265b1e532ca0573e3b6
Sarah Ibrahim <saraibrahim4121@gmail.com>
2022-10-22 20:39:00 +0300

    Done

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,23 @@
-// create your App component here
+import React,{useState, useEffect} from "react";
+
+function App(){
+const [images, setImages] = useState([]);
+const [isLoded, setIsLoded]=useState(false)
+  useEffect(()=>{
+  fetch("https://dog.ceo/api/breeds/image/random")
+  .then((response)=>response.json())
+  .then((data)=>{
+    setImages(data.message);
+    setIsLoded(true);
+  });},[])
+  
+  if(!isLoded) return <p>Loading...</p>
+  return (
+    <div>
+    {<img src={images} alt="A Random Dog"/>}
+    </div>
+    
+  )
+}
+
+export default App;
